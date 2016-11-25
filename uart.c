7c3cda6fb16d49df7ab74b9e961026eddb7ba3ba
hsmouc <heshiming001@icloud.com>
2016-11-25 14:20:26 +0000

    add mysql functions

--- uart.c
+++ uart.c
@@ -5,18 +5,20 @@
 #include <string.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
-
 #define ID_LENTH 12
+MYSQL           mysql;
+MYSQL_RES       *res = NULL;
+MYSQL_ROW       row;
+int user_num;
 struct User{
 	int userID[ID_LENTH];
-	int returnFlag;
+	char *returnFlag;
 	char *name;
-}User[2];
+}User[100];
 
-int j;
 int idAuth(int *testID) {
-	int i,k = 0;
-	for(j = 0; j < 2; j++) {
+	int i,j,k = 0;
+	for(j = 0; j < user_num; j++) {
 		for(i = 0; i < ID_LENTH; i++) {
 			if(*(User[j].userID+i) != *(testID+i)) {
 				k = 0;
@@ -24,39 +26,40 @@ int idAuth(int *testID) {
 			}
 			k++;
 		}
-		if(k == 12) 
+		if(k == ID_LENTH) 
 			break;
 	}
-	if(k == 12)
+	if(k == ID_LENTH)
 		return j;
 	else
 		return -1;
 }
+
+int db_connect() {
+        if (NULL == mysql_init(&mysql)) {
+                printf("mysql_init(): %s\n", mysql_error(&mysql));
+                return -1;
+        }
+        if (NULL == mysql_real_connect(&mysql,
+                        "localhost",
+                        "root",
+                        "ubuntu",
+                        "umbrella_db",
+                        0,
+                        NULL,
+                        0)) {
+                printf("mysql_real_connect(): %s\n", mysql_error(&mysql));
+                return -1;
+        }
+        printf("Connected to MySQL \n");
+}
 	
 int read_db() {  
-    	MYSQL           mysql;  
-    	MYSQL_RES       *res = NULL;  
-    	MYSQL_ROW       row;  
     	char            *query_str = NULL;  
-    	int             rc, i, j, fields;  
+    	int             rc,i,j,fields;  
     	int             rows;  
-    	if (NULL == mysql_init(&mysql)) {  
-        	printf("mysql_init(): %s\n", mysql_error(&mysql));  
-        	return -1;  
-    	}  
-    	if (NULL == mysql_real_connect(&mysql,  
-                	"localhost",  
-                	"root",  
-                	"ubuntu",  
-                	"umbrella_db",  
-                	0,  
-                	NULL,  
-                	0)) {  
-        	printf("mysql_real_connect(): %s\n", mysql_error(&mysql));  
-        	return -1;  
-    	}  
-    	printf("1. Connected MySQL successful! \n");  
-    	query_str = "select * from users";  
+    	db_connect();
+	query_str = "select * from users";  
     	rc = mysql_real_query(&mysql, query_str, strlen(query_str));  
     	if (0 != rc) {  
         	printf("mysql_real_query(): %s\n", mysql_error(&mysql));  
@@ -68,17 +71,37 @@ int read_db() {
          	return -1;  
     	}  
     	rows = mysql_num_rows(res);  
-    	printf("The total rows is: %d\n", rows);  
-    	fields = mysql_num_fields(res);  
-    	printf("The total fields is: %d\n", fields);  
- 	for(j = 0; j < 2; j++) {
+    	fields = mysql_num_fields(res);
+	user_num = rows;  
+    	printf("The total users is: %d\n", rows);
+ 	for(j = 0; j < rows; j++) {
 		row = mysql_fetch_row(res);
 		for(i = 1; i < 13; i++) {
 			*(User[j].userID+i-1) = atoi(row[i]);
 			User[j].name = row[13];
-			User[j].returnFlag = atoi(row[14]);		
+			User[j].returnFlag = row[14];		
 		}
 	}
+	mysql_close(&mysql);
+	return 0;
+}
+
+int write_db(int id,const char *re_state) {
+	char 		*str_1 = NULL;
+	char 		*str_2 = NULL;
+	char 		*str;
+	int             rc;
+	str_1 = "UPDATE users SET return_state=";
+	str_2 = " WHERE id_0=";
+	char temp[10];
+	db_connect();
+	sprintf(temp,"%d",id+1);
+	strcat(strcat(strcat(strcpy(str,str_1),re_state),str_2),temp);
+	rc = mysql_real_query(&mysql, str, 45);
+        if (0 != rc) {
+                printf("mysql_real_query(): %s\n", mysql_error(&mysql));
+                return -1;
+        }
 	mysql_close(&mysql);
 	return 0;
 }
@@ -87,9 +110,9 @@ int main(){
 	int fd;
 	char data[] = {0x03,0x08,0xC1,0x20,0x02,0x00,0x00,0x17};
 	int readData;
-	int ID[12];
+	int ID[ID_LENTH];
 	int i;
-	struct timeval tpstart[2],tpend[2];
+	struct timeval tpstart[3],tpend[3];
 	unsigned long diff; 
 	wiringPiSetup();
 	pinMode(0,OUTPUT);
@@ -110,25 +133,24 @@ int main(){
 				readData = serialGetchar(fd);
 				*(ID+i) = readData;
 			}
-
 			if(idAuth(ID) == -1){
 				system("clear");
 				printf("Permission denied!\n");
 				digitalWrite(0,LOW);
-				delay(6000);
-				system("clear");
-				printf("Copyright OUC:Industrial Automation\n");
+				delay(4000);system("clear");
 			}
 			else{
-				if(User[idAuth(ID)].returnFlag == 0){
+				printf("%s\n",User[1].returnFlag);
+				delay(4000);
+				if(strcmp(User[idAuth(ID)].returnFlag,"0") == 0) {//User[idAuth(ID)].returnFlag == "0"){
 					gettimeofday(&tpstart[idAuth(ID)],NULL);
 					system("clear");
 					printf("Take Your Umbrella!,%s\n",User[idAuth(ID)].name);
-					User[idAuth(ID)].returnFlag = 1;	
+					User[idAuth(ID)].returnFlag = "1";
+					if(write_db(idAuth(ID),"1")==0) printf("Update is done\n");
+					delay(3000);system("clear");
 					digitalWrite(0,HIGH);
-					delay(6000);
-					system("clear");
-					printf("Copyright OUC:Industrial Automation\n");
+					delay(4000);system("clear");
 					digitalWrite(0,LOW);
 				}
 				else{
@@ -136,10 +158,12 @@ int main(){
 					diff = (tpend[idAuth(ID)].tv_sec-tpstart[idAuth(ID)].tv_sec);
 					system("clear");
 					printf("%s:have a nice day!,time= %lu Sec\n",User[idAuth(ID)].name,diff);
-					delay(6000);
-					system("clear");
-					printf("Copyright OUC:Industrial Automation\n");
-					User[idAuth(ID)].returnFlag = 0;
+					User[idAuth(ID)].returnFlag = "0";
+					if(write_db(idAuth(ID),"0")==0) printf("Update is done\n");
+					delay(3000);system("clear");
+                                        digitalWrite(0,HIGH);
+                                        delay(4000);system("clear");
+                                        digitalWrite(0,LOW);				
 				}
 			}
 		}	
